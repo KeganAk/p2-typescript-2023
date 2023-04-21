0451ddd7190125ecfa60a659ed1a7291bac33472
Kegan <kegan.michael.akinsanmi@estudiantat.upc.edu>
2023-04-21 21:40:49 +0200

    render movies

---
+++ renderr.ts
@@ -0,0 +1,59 @@
+import { Movie } from "./moviess.js";
+
+const head = (title: string) => `
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Movies</title>
+  <style>
+    body {
+      margin: 0;
+      padding: 0;
+    }
+    .movie {
+      font-family: sans-serif;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      padding: .4rem;
+      border-bottom: 1px solid #ddd;
+    }
+    .movie img {
+      width: 3rem;
+      height: 3rem;
+      border-radius: 50%;
+      margin-right: 0.7rem;
+    }
+    .movie .name {
+      font-weight: bold;
+    }
+    .movie .genres {
+      font-family: monospace;
+    }
+  </style>
+</head>`;
+
+const renderMovies = (movies: Array<Movie>) => {
+  let html = "";
+  for (const movie of movies) {
+    html += `<div class="movie">
+      <img src="${movie.poster_path}" />
+      <div class="data">
+        <div class="name">${movie.original_title}</div>
+        <div class="genres">${movie.genres}</div>
+      </div>
+    </div>`;
+  }
+  return html;
+}
+
+export const render = (movies: Array<Movie>) => {
+  return `
+<html>
+  ${head("Movie List")}
+  <body>
+    ${renderMovies(movies)}
+  </body>
+</html>`;
+};
