430424dd13bef9a3ea896141f237ec64b5d8d970
Kegan <kegan.michael.akinsanmi@estudiantat.upc.edu>
2023-04-21 21:49:25 +0200

    main

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 import { writeFile } from "fs/promises";
-import { render } from "./render.js";
-import { loadUsers } from "./users.js"; //moviess
+import { render } from "./renderr.js"; //renderr
+import { loadMovies } from "./moviess.js"; //moviess
 
-const users = await loadUsers(100);
-const html = render(users);
-await writeFile('users.html', html);
+const movies = await loadMovies(3);
+const html = render(movies);
+await writeFile('movies.html', html);
 
