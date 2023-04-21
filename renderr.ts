3155f22f046cdbe3739fa05cfed553a6284288e5
Kegan <kegan.michael.akinsanmi@estudiantat.upc.edu>
2023-04-21 21:45:51 +0200

    movie render

--- renderr.ts
+++ renderr.ts
@@ -5,7 +5,7 @@ const head = (title: string) => `
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Movies</title>
+  <title>${title}</title>
   <style>
     body {
       margin: 0;
