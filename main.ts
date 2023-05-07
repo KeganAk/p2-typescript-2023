a96e15c11fae387a376c0eea7ae70078c31a4ea2
Kegan <kegan.michael.akinsanmi@estudiantat.upc.edu>
2023-05-07 21:05:28 +0200

    fix del problema de los dos puntos

--- main.ts
+++ main.ts
@@ -1,13 +1,17 @@
 import { writeFile } from "fs/promises";
 import { render, movieRender } from "./renderr.js"; //renderr
-import { loadMovies, loadMovie } from "./moviess.js"; //moviess
+import { loadMovies } from "./moviess.js"; //moviess
+import { loadMovie } from "./moviee.js" //moviee 
 
-const movies = await loadMovies(3);
+const movies = await loadMovies(6);
 const html = render(movies);
 await writeFile('movies.html', html);
 
 for (const oneMovie of movies){
     const movie = await loadMovie(oneMovie.id);
     const html = movieRender(movie);
-    await writeFile(`./movie/${oneMovie.title}.html`, html);
+
+    const newString = oneMovie.title.replace(/:/g, "");
+
+    await writeFile(`./movie/${newString}.html`, html);
 }
